1cd4cd2b249c5716c566861bf85d07d3e734879d
Ben Chuanlong Du <duchuanlong+git@gmail.com>
2014-09-04 21:25:02 -0400

    fixed the tab stop index bug

--- ultisnips.py
+++ ultisnips.py
@@ -3,25 +3,34 @@
 import warnings
 import re
 
-def parseArgs(args, tab_stop_index):
+def parse_args(args, reset):
+    if reset:
+        parse_args.__tab_stop_index__  = 1
+    #end if
     if args == "":
         return ""
     #end if
     if args.startswith(","):
-        return "," + parseArgs(args[1:], tab_stop_index)
+        return "," + parse_args(args[1:], False)
     #end if
     if args.startswith(" "):
-        return " " + parseArgs(args[1:], tab_stop_index)
+        return " " + parse_args(args[1:], False)
     #end if
     if args.startswith("["):
         if args.endswith("]"):
-            return "${" + str(tab_stop_index) + ":" + parseArgs(args[1:-1], tab_stop_index+1) + "}"
+            snip = "${" + str(parse_args.__tab_stop_index__) + ":" 
+            parse_args.__tab_stop_index__ += 1
+            snip += parse_args(args[1:-1], False) + "}"
+            return snip
         #end if
         warnings.warn('Mal-formatted brackets in "' + args + '"!')
         return ""
     #end if
     index = endOfFirstArg(args)
-    return "${" + str(tab_stop_index) + ":" + args[:index] + "}" + parseArgs(args[index:], tab_stop_index+1)
+    snip = "${" + str(parse_args.__tab_stop_index__) + ":" + args[:index] + "}" 
+    parse_args.__tab_stop_index__ += 1
+    snip += parse_args(args[index:], False)
+    return snip
 #end def
 
 def endOfFirstArg(args):
@@ -86,10 +95,11 @@ def methodSnippet(line, prefix, trigger, extraSymbol, triggerInTabStop):
             snip = 'snippet ' + tm + ' "' + tm + '" w\n' 
         #end if
         if triggerInTabStop:
-            snip += "${1:" + trigger + "}" + method + "(" + parseArgs(line[begin+1:end], 2) + ")\n"
+            temp = method + "(" + parse_args(line[begin+1:end], True) + ")\n"
+            snip += "${" + str(parse_args.__tab_stop_index__) + ":" + trigger + "}" + temp
             snip += "endsnippet\n"
         else:
-            snip += tm + "(" + parseArgs(line[begin+1:end], 1) + ")\n"
+            snip += tm + "(" + parse_args(line[begin+1:end], True) + ")\n"
             snip += "endsnippet\n"
         #end if
         return snip
